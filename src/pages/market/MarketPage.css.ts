e7c5b5735727db0ede88d1706c061abd88385c3a
sungyun6789 <sungyun5423@gmail.com>
2024-04-24 12:06:38 +0900

    feat: 시세 목록에서 코인게코 데이터 가져오고 테이블, 필터 컴포넌트 연결

---
+++ src/pages/market/MarketPage.css.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const MoreButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 25px 0;
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #f0f0f0;
+  background-color: #fff;
+  cursor: pointer;
+`;
--- src/pages/market/MarketPage.tsx
+++ src/pages/market/MarketPage.tsx
@@ -1,9 +1,62 @@
+import axios from 'axios';
+import MarketTable from '../../components/market-table';
 import NavigationBar from '../../components/navigation-bar';
+import { useQuery } from '@tanstack/react-query';
+import type { Market } from '../../types';
+import { useEffect, useState } from 'react';
+import { MoreButton } from './MarketPage.css';
+import MarketFilter from '../../components/market-filter';
 
 const MarketPage = () => {
+  const [list, setList] = useState<Market[]>();
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(50);
+  const [currencyType, setCurrencyType] = useState<'krw' | 'usd'>('krw');
+  const [viewType, setViewType] = useState<'all' | 'bookmark'>('all');
+
+  const { data } = useQuery({
+    queryKey: ['/coins/markets', page, pageSize],
+    queryFn: async () => {
+      const { data } = await axios.get<Market[]>('https://api.coingecko.com/api/v3/coins/markets', {
+        params: {
+          vs_currency: currencyType,
+          per_page: pageSize,
+          page: page,
+          price_change_percentage: '1h,24h,7d',
+        },
+      });
+      return data;
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      setList((l) => [...(l ?? []), ...data]);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    setPage(1);
+    setList(data);
+  }, [currencyType, pageSize]);
+
+  const onClickMore = () => {
+    setPage(page + 1);
+  };
+
   return (
     <>
       <NavigationBar />
+      <MarketFilter
+        viewType={viewType}
+        setViewType={setViewType}
+        currencyType={currencyType}
+        setCurrencyType={setCurrencyType}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+      />
+      <MarketTable data={list} currencyType={currencyType} />
+      <MoreButton onClick={onClickMore}>+ 더보기</MoreButton>
     </>
   );
 };
